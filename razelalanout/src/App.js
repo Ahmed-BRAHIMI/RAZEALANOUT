f17d35b8d45089eca69e5d725645cfb7896a0779
Ahmed BRAHIMI <97116751+Ahmed-BRAHIMI@users.noreply.github.com>
2024-01-02 01:11:39 +0100

    cart added

--- razelalanout/src/App.js
+++ razelalanout/src/App.js
@@ -1,25 +1,47 @@
-
+import { useState } from 'react';
 import NavBar from './NavBar';
 import List from './List';
 import './App.css';
 
 
-function App() {
+function MyButton () {
+   const  [count, setCount] = useState(0);
+
+   function handleClick() {
+      setCount(count + 1);
+   }
+
+  return (
+    <button onClick={handleClick}>
+      Clicked {count} times
+
+    </button>
+  )
+};
+
+export default function App() {
     const epices = ['cumin', 'paprika', 'curcuma', 'poivre'];
 
   return(
     <>            
       <NavBar title="Le vrai gout des choses et rien de plus!"/>
+      
   
       <div className="container">
       <List
           title="Epices"
           items={epices[2]}
       />
+      
+    <div>
+      <h1>Add to cart</h1>
+      <MyButton/>
+    </div>
+    
       </div>
       
     </>
-  )
+      )
 }
 
-export default App;
+
--- razelalanout/src/NavBar.js
+++ razelalanout/src/NavBar.js
@@ -1,8 +1,11 @@
+
+
 function NavBar(props){
     return(
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div className="container">
             <a className="navbar-brand" href="#">RAZE AL ANOUT: {props.title}</a>
+            
         </div>
         </nav>
 
